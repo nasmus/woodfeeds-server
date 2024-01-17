e2f0da171f01e5239aceb935b56214ab0ea1b817
nasmus <nasmus.shahadat@gmail.com>
2024-01-17 23:04:14 +0600

    image upload fix

--- imageUpload.js
+++ imageUpload.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
       cb(
         null,
-        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        file.originalname
       );
     },
   });
@@ -23,8 +23,7 @@ imageUpload.post(
     '/upload',
     multipleUpload.array('multipleImage', 5),
     (async(req,res) => {
-        const multipleImage = req.files.map((file) => file.filename);
-
+        const multipleImage = await req.files.map((file) => file.filename);
         res.send(multipleImage)
     }) 
 )
